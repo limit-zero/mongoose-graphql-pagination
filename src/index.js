ae5289993e4f8457a0c24ffac101f1e6345179d7
Jacob Bare <jacob@limit0.io>
2018-05-04 13:24:05 -0500

    Add index export

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const Pagination = require('./pagination');
+
+module.exports = { Pagination };
---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+const models = require('../src/index');
+
+describe('index', function() {
+  it('should be an object with the Pagination object.', function(done) {
+    expect(models).to.be.an('object').with.all.keys(
+      'Pagination',
+    );
+    done();
+  });
+});
