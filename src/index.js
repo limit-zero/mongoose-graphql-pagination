463a4bb0572f9ca4eed54225012889037fbf8d48
Jacob Bare <jacob@limit0.io>
2018-05-22 12:19:18 -0500

    Move elastic search pagination to root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Pagination = require('./pagination');
-const ElasticPagination = require('./elasticsearch/pagination');
+const ElasticPagination = require('./search-pagination');
 const TypeAhead = require('./type-ahead');
 const paginationResolvers = require('./resolvers');
 
