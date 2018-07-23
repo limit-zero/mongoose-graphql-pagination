fdcbc9b2bd4c88465ef6d0be722d5ae8a5d4f9b3
Jacob Bare <jacob@limit0.io>
2018-07-23 08:00:37 -0500

    Add paginable mongoose plugin

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const Pagination = require('./pagination');
 const ElasticPagination = require('./search-pagination');
 const TypeAhead = require('./type-ahead');
 const paginationResolvers = require('./resolvers');
+const plugins = require('./plugins');
 
 module.exports = {
   Pagination,
   TypeAhead,
   ElasticPagination,
   paginationResolvers,
+  plugins,
 };
